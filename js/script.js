590d9e999b3ad0fb29022bfe5fca3d2b78dd0c12
nikotheflow <bboyfoxxx@yandex.ru>
2022-03-07 21:01:43 +0300

    Add custom select input

--- js/script.js
+++ js/script.js
@@ -2,32 +2,101 @@ const body = document.querySelector('body'),
       anchors = document.querySelectorAll('.anchor'),
       header = document.querySelector('.section-header'),
       navigation = document.querySelector('.nav'),
+      lockPadding = document.querySelectorAll('.lock-padding'),
       catalogImages = document.querySelectorAll('.catalog_item-image'),
       catalogTitles = document.querySelectorAll('.catalog_item-title'),
-      burgerButton = document.querySelector('.button__burger'),
-      confirmOrderButton = document.querySelector('.confirm-order-button'),
-      countButton = document.querySelector('.section-rations_count-button'),
-      orderButtons = document.querySelectorAll('.catalog_item-button'),      
+      burgerButton = document.querySelector('.button__burger'),     
       accordionHeaders = document.querySelectorAll('.accordion_header'),
       compositionsDesktop = document.querySelector('.section-barf_compositions__desktop'),
       compositionsMobile = document.querySelector('.section-barf_compositions__mobile'),
       catalogDesktop = document.querySelector('.section-rations_catalog__desktop'),
       catalogMobile = document.querySelector('.section-rations_catalog__mobile'),
-      catalogPopup = document.querySelector('.popup__catalog'),
-      countPopup = document.querySelector('.popup__count'),
+      popupLinks = document.querySelectorAll('.popup-link'),
+      contentLinks = document.querySelectorAll('.content-link'),
+      popupCloseButtons = document.querySelectorAll('.popup_close-button'),
       popupHeader = document.querySelector('.popup_header'),
       popupItem = document.querySelector('.popup_description__item-info'),
       popupOrder = document.querySelector('.popup_description__order'),
-      popupThanks = document.querySelector('.popup_description__thanks');
+      popupThanks = document.querySelector('.popup_description__thanks'),
+      selectHeaders = document.querySelectorAll('.select__header'),
+      selectItems = document.querySelectorAll('.select__item'),
+      timeout = 200; // time to open popup
 
-const popupLinks = document.querySelectorAll('.popup-link'),
-      contentLinks = document.querySelectorAll('.content-link'),
-      lockPadding = document.querySelectorAll('.lock-padding'),
-      popupCloseButtons = document.querySelectorAll('.popup_close-button');
+// create swipers
 
-const timeout = 200; // time to open popup
+const compositionSwiper = new Swiper('.section-barf_compositions__mobile', {
+  direction: 'horizontal',
+  loop: false,
 
-// popups
+  pagination: {
+    el: '.swiper-pagination'
+  }
+});
+
+const catalogSwiper = new Swiper('.section-rations_catalog__mobile', {  
+  slidesPerView: "auto",
+  spaceBetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// add listeners
+
+window.addEventListener('DOMContentLoaded', () => {
+  closeBurgerMenu()
+  showCompositions();
+  showCatalog()
+})
+
+window.addEventListener('resize', () => {
+  closeBurgerMenu()
+  showCompositions();
+  showCatalog()
+})
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset >= document.documentElement.clientHeight) {
+    header.classList.add('section-header__fixed');
+  } else {
+    header.classList.remove('section-header__fixed');
+  }
+})
+
+burgerButton.addEventListener('click', function() {
+  navigation.classList.toggle('active');
+  burgerButton.classList.toggle('active');
+  body.classList.toggle('lock');
+
+  document.querySelection()
+})
+
+// anchors navigation
+
+// seamless.polyfill(); //smooth scroll for safari
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault()
+    closeBurgerMenu()
+
+    let sectionID = anchor.getAttribute('href');
+
+    if (sectionID === '#barf') {
+      window.scrollTo({
+        top: document.documentElement.clientHeight,
+        behavior: 'smooth'
+      })
+    } else {
+      document.querySelector(sectionID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'    
+      })
+    }
+  })
+})
 
 if (popupLinks.length > 0) {
   for (let index = 0; index < popupLinks.length; index++) {
@@ -62,6 +131,50 @@ if (contentLinks.length > 0) {
   }
 }
 
+accordionHeaders.forEach(header => header.addEventListener('click', toggleAccordion));
+
+selectHeaders.forEach(selectHeader => {
+  selectHeader.addEventListener('click', toggleSelect)
+})
+
+selectItems.forEach(selectItem => {
+  selectItem.addEventListener('click', chooseSelect)
+})
+
+function hideAllInfo() {
+  popupHeader.style.display = "none";
+  popupItem.style.display = "none";
+  popupOrder.style.display = "none";
+  popupThanks.style.display = "none";
+}
+
+// compositions and catalog sections variants for mobile and desktop
+
+function showCompositions() {
+  if (document.documentElement.clientWidth >= 575.98) {
+    compositionsMobile.style.display = "none";
+    compositionsDesktop.style.display = "flex";
+  } else {    
+    compositionsDesktop.style.display = "none";
+    compositionsMobile.style.display = "flex";
+  }
+}
+
+function showCatalog() {
+  if (document.documentElement.clientWidth >= 575.98) {
+    catalogMobile.style.display = "none";
+    catalogDesktop.style.display = "flex";
+  } else {    
+    catalogDesktop.style.display = "none";
+    catalogMobile.style.display = "flex";
+  }
+}
+
+function closeBurgerMenu() {
+  navigation.classList.remove('active');
+  burgerButton.classList.remove('active');
+  body.classList.remove('lock');
+}
 
 function checkContent(popupContent) {
   hideAllInfo()
@@ -123,122 +236,22 @@ function bodyUnlock() {
   }, timeout);
 }
 
-// create swipers
-
-const compositionSwiper = new Swiper('.section-barf_compositions__mobile', {
-  direction: 'horizontal',
-  loop: false,
-
-  pagination: {
-    el: '.swiper-pagination'
-  }
-});
-
-const catalogSwiper = new Swiper('.section-rations_catalog__mobile', {  
-  slidesPerView: "auto",
-  spaceBetween: 20,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-
-// add listeners
-
-window.addEventListener('DOMContentLoaded', () => {
-  closeBurgerMenu()
-  showCompositions();
-  showCatalog()
-})
-
-window.addEventListener('resize', () => {
-  closeBurgerMenu()
-  showCompositions();
-  showCatalog()
-})
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset >= document.documentElement.clientHeight) {
-    header.classList.add('section-header__fixed');
-  } else {
-    header.classList.remove('section-header__fixed');
-  }
-})
-
-burgerButton.addEventListener('click', function() {
-  navigation.classList.toggle('active');
-  burgerButton.classList.toggle('active');
-  body.classList.toggle('lock');
-
-  document.querySelection()
-})
-
-accordionHeaders.forEach(header => header.addEventListener('click', toggleAccordion));
-// anchors navigation
-
-// seamless.polyfill(); //smooth scroll for safari
-
-anchors.forEach((anchor) => {
-  anchor.addEventListener('click', (e) => {
-    e.preventDefault()
-    closeBurgerMenu()
-
-    let sectionID = anchor.getAttribute('href');
-
-    if (sectionID === '#barf') {
-      window.scrollTo({
-        top: document.documentElement.clientHeight,
-        behavior: 'smooth'
-      })
-    } else {
-      document.querySelector(sectionID).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'    
-      })
-    }
-  })
-})
-
-function hideAllInfo() {
-  popupHeader.style.display = "none";
-  popupItem.style.display = "none";
-  popupOrder.style.display = "none";
-  popupThanks.style.display = "none";
-}
-
-// compositions and catalog sections variants for mobile and desktop
-
-function showCompositions() {
-  if (document.documentElement.clientWidth >= 575.98) {
-    compositionsMobile.style.display = "none";
-    compositionsDesktop.style.display = "flex";
-  } else {    
-    compositionsDesktop.style.display = "none";
-    compositionsMobile.style.display = "flex";
-  }
-}
-
-function showCatalog() {
-  if (document.documentElement.clientWidth >= 575.98) {
-    catalogMobile.style.display = "none";
-    catalogDesktop.style.display = "flex";
-  } else {    
-    catalogDesktop.style.display = "none";
-    catalogMobile.style.display = "flex";
-  }
-}
-
-function closeBurgerMenu() {
-  navigation.classList.remove('active');
-  burgerButton.classList.remove('active');
-  body.classList.remove('lock');
-}
-
 function toggleAccordion() {
   let thisItem = this.parentNode;
 
   thisItem.classList.toggle('accordion_item__active');
 }
 
+function toggleSelect() {
+  this.parentElement.classList.toggle('active');
+}
+
+function chooseSelect() {
+  let text = this.innerText,
+      select = this.closest('.select'),
+      currentText = select.querySelector('.select__current');
+  
+  currentText.style.opacity = '1';
+  currentText.innerText = text;
+  select.classList.remove('active');
+}
